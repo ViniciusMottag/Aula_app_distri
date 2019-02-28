afacc688e216db36ac15937f40e22dc4a9086cc9
Vinicius Motta Grossi <1800842@SALAS.AULAS>
2019-02-28 11:35:13 -0300

    aula1

---
+++ funcoes_banco/estruturado/MeusModulos/ContaCorrente.py
@@ -0,0 +1,20 @@
+from . import Clientes
+
+class LimiteCreditoExcedidoException(Exception):
+    pass #nao precisa fazer nada nessa classe, ela ja define a excessao nova
+
+#crie aqui uma nova excessao LimiteTransferenciaExcedidoException
+
+def credito(valor, cliente, limite_credito, limite_transferencia):
+    cliente['saldo'] = valor + cliente['saldo']
+
+def debito(valor, cliente, limite_credito, limite_transferencia):
+    saque_max = cliente['saldo']+limite_credito
+    if valor > saque_max:
+        raise LimiteCreditoExcedidoException
+    cliente['saldo'] = cliente['saldo'] - valor
+    
+def transferencia(lista_clientes,id_cliente_doador,id_cliente_receptor,valor,limite_credito,limite_transferencia):
+    Clientes.minhafuncao()
+    pass
+
